3583e37b993dc157b3a61efb06cdb7bf1d3f62ef
Charlotte Stone <charlie.stone649@gmail.com>
2023-09-27 11:33:36 +0000

    Added the footer section, applied some javascript to the contact buttons so phone and email are applied to the user's clipboard when clicked.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+function copyEmail() {
+  console.log("click")
+    navigator.clipboard.writeText("charlie.stone649@gmail.com")
+}
+
+function copyNum() {
+  console.log("click")
+    navigator.clipboard.writeText("+44 07369 279 305")
+}
