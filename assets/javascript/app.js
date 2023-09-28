110585bd05b5669e47dc16ce97d9b9af153ff429
Charlotte Stone <charlie.stone649@gmail.com>
2023-09-28 07:32:51 +0000

    Add animations to the site on scrolling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,3 +43,18 @@ function themeChanger() {
   themeChange = !themeChange
 }
 
+//scroll animations
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add("show")
+    } else {
+      entry.target.classList.remove("show")
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
