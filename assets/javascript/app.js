94a40f19f3870cf5b80d15e6465bb10d8ccf8668
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-10 09:35:30 +0000

    Fixed visual glitches.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,7 @@ function copyNum() {
 let mainBG = document.getElementById("page-background")
 let theme = document.getElementsByClassName("section-alt")
 let cards = document.getElementsByClassName("card")
+let regular = document.getElementsByClassName("regular")
 
 let themeChange = false
 
@@ -23,19 +24,28 @@ function themeChanger() {
     for(var i = 0; i < theme.length; i++){
       theme[i].style.backgroundColor = "#575757"
       theme[i].style.color = "#FFF"
+      
     }
     for(var i = 0; i < cards.length ; i++){
       cards[i].style.backgroundColor = "#000"
     }
+    for(var i = 0; i < regular.length; i++){
+      regular[i].style.color = "#FFF"
+    }
+
   } else {
     //lightmode code
     mainBG.style.backgroundColor = "#FFF"
     for(var i = 0; i < theme.length; i++){
       theme[i].style.backgroundColor = "#F9FAFB"
       theme[i].style.color = "#858C96"
+      regular[i].style.color = "#4a4d50"
     }
     for(var i = 0; i < cards.length ; i++){
       cards[i].style.backgroundColor = "#FFF"
+    }
+    for(var i = 0; i < regular.length; i++){
+      regular[i].style.color = "#4a4d50"
     }
   }
   themeChange = !themeChange
