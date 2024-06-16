9f7e451c6a148e4cb605cb32336f691f9b7e28f2
Charlotte Stone <charlie.stone649@gmail.com>
2024-06-16 07:50:53 +0100

    Fuck Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
+import Home from './components/home';
 
 function App() {
 
   return (
     <>
+      <Home />
     </>
+    )
 }
 
 export default App
